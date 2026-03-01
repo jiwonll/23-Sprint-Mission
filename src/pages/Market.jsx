5c27ffb4046801b3954301d304aba0b23704a19a
jiwonll <yoonjw4966@hanyang.ac.kr>
2026-03-01 23:31:38 +0900

    Sync project to GitHub

---
+++ src/pages/Market.jsx
@@ -0,0 +1,26 @@
+import Header from '../components/Header';
+import BestSection from '../Routes/BestSection';
+import ProdutList from '../Routes/ProductList';
+
+export default function Market(){
+    const [items, setItems] = useState([]);
+    const [bestItems, setBestItems] = useState([]);
+    const [page, setPage] = useState(1);
+    const [isLoading, setIsLoading] = useState(true);
+
+
+    return (
+        <>
+        <Header/>
+        <main>
+            <BestSection></BestSection>
+            <ProdutList></ProdutList>
+
+        </main>
+        
+        
+        
+        
+        </>
+    )
+}
